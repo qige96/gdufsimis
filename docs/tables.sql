305edfadc6d21e8c973d4f0eeb3e6e3526e6f4fe
qige96 <zrq96@163.com>
2017-04-23 20:42:24 +0800

    add tables.sql file

---
+++ docs/tables.sql
@@ -0,0 +1,49 @@
+create table user(
+	stu_id int(12) unsigned key ,
+	name var(50),
+	nick_name varchar(50),
+	password varchar(32),
+	gender tinyint,
+	signature varchar(100)
+);
+
+create table resource(
+	resource_id int unsigned key auto_increment,
+	title varchar(50),
+	description varchar(100),
+	contributor varchar(50),
+	upload_date int(11) unsigned,
+	download_times int unsigned,
+	download_link varchar(100)
+);
+
+create table article(
+	article_id int unsigned key auto_increment,
+	title varchar(50),
+	content text,
+	author varchar(50),
+	create_date int(11),
+);
+
+create table article_imgs(
+	id int unsigned key auto_increment,
+	article_id int unsigned,
+	img_link varchar(100)
+);
+
+create table topic(
+	topic_id int unsigned key auto_increment,
+	title varchar(100),
+	description text,
+	raiser varchar(50),
+	dateline int(11) unsigned,
+);
+
+create table topic_response(
+	id bigint unsigned key auto_increment,
+	topic_id int unsigned,
+	responsor varchar(50),
+	dateline int(11) unsigned,
+	response_body text
+)
+
